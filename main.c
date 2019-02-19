1ea89c2adeb8ce2a5dd9aeb94c3809d0424b7e5b
Aleksander Dykman <a.dykman@EAE.local>
2019-02-19 18:01:45 +0100

    SPI configuration fixed

--- main.c
+++ main.c
@@ -33,18 +33,18 @@ int main()
 
 void Init()
 {
-  //Clocks
+  /******  Clocks **********************************************************************************/
   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN | RCC_AHB1ENR_GPIODEN | RCC_AHB1ENR_GPIOAEN;
   RCC->APB2ENR |= RCC_APB2ENR_TIM1EN | RCC_APB2ENR_SPI1EN;
-  //LEDS
+  /******  LEDS**********************************************************************************/
   GPIOD->MODER |= GPIO_MODER_MODER12_0 | GPIO_MODER_MODER13_0 | GPIO_MODER_MODER14_0 | GPIO_MODER_MODER15_0;
-  //Motor Direction
+  /******  Motor Direction **********************************************************************************/
   GPIOE->MODER |= GPIO_MODER_MODER9_0 | GPIO_MODER_MODER13_0;//PE9, PE13
   GPIOD->MODER |= GPIO_MODER_MODER0_0;//PD0
   GPIOA->MODER |= GPIO_MODER_MODER10_0;//PA10
-  //Systick - every 1ms
+  /******  Systick - every 1ms **********************************************************************************/
   SysTick_Config(16000);
-  //TIM1 - Motor PWM
+  /******  TIM1 - Motor PWM **********************************************************************************/
   GPIOA->MODER |= GPIO_MODER_MODER8_1;//PA8
   GPIOA->AFR[1] = 0x1;
   GPIOE->MODER |= GPIO_MODER_MODER11_1;//PE11
@@ -57,10 +57,12 @@ void Init()
   TIM1->PSC = 15;//10kHz
   TIM1->EGR |= TIM_EGR_UG;
   TIM1->CR1 |= TIM_CR1_ARPE | TIM_CR1_CEN;
-  //SPI1 - L3GD20 (Gyro)
-  GPIOA->MODER |= GPIO_MODER_MODER5_1 | GPIO_MODER_MODER6_1 |GPIO_MODER_MODER7_1;//PA5, PA6, PA7
+  /******  SPI1 - L3GD20 (Gyro) **********************************************************************************/
+  GPIOA->MODER |= GPIO_MODER_MODER5_1 | GPIO_MODER_MODER6_1 |GPIO_MODER_MODER7_1;//PA5(SCK), PA6(MISO), PA7(MOSI)
   GPIOA->AFR[0] |= 0x5<<5 | 0x5<<6 | 0x5<<7;
-  //SPI1->
+  GPIOE->MODER |= GPIO_MODER_MODER3_0;//PE3 (CS)
+  GPIOE->PUPDR |= GPIO_PUPDR_PUPDR3_0;//Pull-Up
+  SPI1->CR1 |= SPI_CR1_SSM | SPI_CR1_SSI | SPI_CR1_MSTR | SPI_CR1_SPE;
 }
 
 void MOTORS (uint8_t direction, uint8_t duty)
